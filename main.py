9d6195d12ebc7f133f6627ebb31692cb7a4f3c89
SanjayCHAUDHARY50 <60917544+SanjayCHAUDHARY50@users.noreply.github.com>
2020-11-03 19:24:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,24 @@ from Histogram import *
 from ColumnChart import *
 from PieChart import *
 def getdata():
-    getdata.GraphNo =  int(input('Enter the Series No. of the graph which you want to use for visualization of your Data : \n 1. Histogram \n 2. Column Chart \n 3. Box Plot Chart \n 4. Pie Chart \n 5. Scatter Plot\n :-'))
+    global GraphNo
+    GraphNo =  int(input('Enter the Series No. of the graph which you want to use for visualization of your Data : \n 1. Histogram \n 2. Column Chart \n 3. Box Plot Chart \n 4. Pie Chart \n 5. Scatter Plot\n :-'))
 
 while(True):
  getdata()
- if getdata.GraphNo == 1:
+ if GraphNo == 1:
     Histogram()
     break
- elif getdata.GraphNo == 2:
+ elif GraphNo == 2:
     ColumnChart()
     break
- elif getdata.GraphNo == 3:
+ elif GraphNo == 3:
     BoxPlotGraph()
     break
- elif getdata.GraphNo == 4:
+ elif GraphNo == 4:
     PieChart()
     break
- elif getdata.GraphNo == 5:
+ elif GraphNo == 5:
     ScatterPlot()
     break
  else: print("Invalid Input")
